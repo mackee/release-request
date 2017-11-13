8e0d75ef9145f3235e271910e4e0e419627cd65a
mackee <macopy123@gmail.com>
2017-11-13 12:29:16 +0900

    add release scripts

--- main.go
+++ main.go
@@ -8,13 +8,16 @@ import (
 	"github.com/hashicorp/logutils"
 )
 
+var Version string
+
 func main() {
 	var configfile, loglevel string
-	var isDryRun, isInit bool
+	var isDryRun, isInit, isShowVersion bool
 	flag.StringVar(&configfile, "config", "config.yml", "specify config file")
 	flag.StringVar(&loglevel, "loglevel", "WARN", "log level. ex.ERROR,WARN,DEBUG")
 	flag.BoolVar(&isDryRun, "dry-run", false, "no create/update pull request")
 	flag.BoolVar(&isInit, "init", false, "generate sample of configuration files")
+	flag.BoolVar(&isShowVersion, "version", false, "show this version")
 	flag.Parse()
 
 	filter := &logutils.LevelFilter{
@@ -23,6 +26,11 @@ func main() {
 		Writer:   os.Stderr,
 	}
 	log.SetOutput(filter)
+
+	if isShowVersion {
+		log.Printf("[WARN] release-request %s", Version)
+		return
+	}
 
 	if isInit {
 		err := GenerateTemplates(configfile)
